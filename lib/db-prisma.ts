12825a21bbd2b081ab131cf6e84826caa1adb294
Simon Sundström <simon.sundstrom@amedia.no>
2025-12-30 20:09:47 +0100

    use neon pg adapter

--- lib/db-prisma.ts
+++ lib/db-prisma.ts
@@ -1,12 +1,7 @@
 import { PrismaClient } from "@/prisma/generated/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
